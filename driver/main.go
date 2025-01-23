f7c27b3f7e1d4b71506d1127940916f82634b598
Justin Traglia <jtraglia@pm.me>
2025-01-23 14:09:41 -0600

    wip

--- driver/main.go
+++ driver/main.go
@@ -15,9 +15,11 @@ import (
 )
 
 const (
-	shmDriverKey = 0                 // Shared memory key
-	shmSize      = 100 * 1024 * 1024 // Shared memory size (100 MiB)
+	shmDriverKey = 1000
+	shmSize      = 100 * 1024 * 1024
 	shmPerm      = 0666
+
+	maxClientNameLength = 32
 
 	socketName = "/tmp/eth-cl-fuzz"
 )
@@ -39,24 +41,7 @@ func generatePseudoRandomData(shmSize int) []byte {
 	return data
 }
 
-/*
-func deleteSharedMemory(shmKey int) error {
-	// Delete existing shared memory if it exists
-	shmId, err := shm.Get(shmKey, 0, shmPerm)
-	if err == nil {
-		fmt.Printf("[driver] Found existing shared memory segment with ID %d, removing it...\n", shmId)
-		_, err := shm.Ctl(shmId, shm.IPC_RMID, nil)
-		if err != nil {
-			fmt.Printf("Failed to remove existing shared memory: %v\n", err)
-			return err
-		} else {
-			fmt.Println("[driver] Successfully removed existing shared memory.")
-		}
-	}
-	return nil
-}
-*/
-
+// cleanupSharedMemory detaches and removes all shared memories.
 func cleanupSharedMemory(clients map[string]*Client, shmId int, shmBuffer []byte) {
 	// Cleanup driver shared memory
 	fmt.Println("[cleanup] Detaching and removing driver shared memory...")
@@ -80,6 +65,7 @@ func cleanupSharedMemory(clients map[string]*Client, shmId int, shmBuffer []byte
 	}
 }
 
+// newSharedMemory creates a new shared memory segment.
 func newSharedMemory(shmKey int) (int, []byte, error) {
 	// Create the shared memory segment
 	shmId, err := shm.Get(shmKey, shmSize, shmPerm|shm.IPC_CREAT|shm.IPC_EXCL)
@@ -87,7 +73,7 @@ func newSharedMemory(shmKey int) (int, []byte, error) {
 		fmt.Printf("Error creating shared memory: %v\n", err)
 		return 0, nil, err
 	}
-	fmt.Printf("[driver] Created shared memory segment with ID %d\n", shmId)
+	fmt.Printf("Created shared memory segment with ID %d\n", shmId)
 
 	// Attach to the shared memory segment
 	shmBuffer, err := shm.At(shmId, 0, 0)
@@ -100,8 +86,8 @@ func newSharedMemory(shmKey int) (int, []byte, error) {
 }
 
 func main() {
-	clients := make(map[string]*Client)
 	mu := &sync.Mutex{}
+	clients := make(map[string]*Client)
 
 	// Handle SIGINT for cleanup
 	signalChan := make(chan os.Signal, 1)
@@ -112,10 +98,8 @@ func main() {
 		fmt.Printf("Error creating input shm: %v\n", err)
 		os.Exit(1)
 	}
-	//defer shm.Dt(shmBuffer)
-	//defer shm.Ctl(shmId, shm.IPC_RMID, nil)
 
-	// Signal cleanup on interrupt
+	// A thread for cleanup
 	go func() {
 		<-signalChan
 		fmt.Println("\n[INFO] Received SIGINT, cleaning up...")
@@ -125,31 +109,32 @@ func main() {
 		os.Exit(0)
 	}()
 
-	var totalTime time.Duration
+	// A thread for status updates
 	var count int
-
+	var totalTime time.Duration
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
 	go func() {
 		for range ticker.C {
 			if count != 0 {
 				average := totalTime / time.Duration(count)
-				fmt.Printf("[driver] Fuzzing Time: %v, Iterations: %v, Average Iteration Time: %v\n", totalTime, count, average)
+				fmt.Printf("Fuzzing Time: %v, Iterations: %v, Average Iteration Time: %v\n", totalTime, count, average)
 			}
 		}
 	}()
 
-	os.Remove(socketName)
+	//os.Remove(socketName)
 	registrationListener, err := net.Listen("unix", socketName)
 	if err != nil {
 		fmt.Printf("Error creating Unix domain socket: %v\n", err)
 		os.Exit(1)
 	}
 	defer registrationListener.Close()
+	defer os.Remove(socketName)
 
-	fmt.Println("[Server] IPC server started. Waiting for clients...")
+	fmt.Println("IPC server started. Waiting for clients...")
 
-	clientShmKey := 1
+	// A thread for client registrations
 	go func() {
 		for {
 			conn, err := registrationListener.Accept()
@@ -159,7 +144,7 @@ func main() {
 			}
 			defer conn.Close()
 
-			clientNameBytes := make([]byte, 32)
+			clientNameBytes := make([]byte, maxClientNameLength)
 			n, err := conn.Read(clientNameBytes)
 			if err != nil {
 				fmt.Printf("Error reading client name: %v\n", err)
@@ -167,36 +152,18 @@ func main() {
 			}
 			clientName := string(clientNameBytes[:n])
 
+			clientShmKey := shmDriverKey + len(clients)
 			clientShmId, clientShmBuffer, err := newSharedMemory(clientShmKey)
 			if err != nil {
 				fmt.Printf("Error creating client output shm: %v\n", err)
 				os.Exit(1)
 			}
-			//defer shm.Dt(clientShmBuffer)
-			//defer shm.Ctl(clientShmId, shm.IPC_RMID, nil)
-			defer func() {
-				fmt.Println("[DEBUG] Detaching shared memory buffer.")
-				if err := shm.Dt(clientShmBuffer); err != nil {
-					fmt.Printf("[ERROR] Failed to detach shared memory buffer: %v\n", err)
-				} else {
-					fmt.Println("[DEBUG] Successfully detached shared memory buffer.")
-				}
-			}()
-
-			defer func() {
-				fmt.Println("[DEBUG] Removing shared memory segment.")
-				if _, err := shm.Ctl(clientShmId, shm.IPC_RMID, nil); err != nil {
-					fmt.Printf("[ERROR] Failed to remove shared memory segment: %v\n", err)
-				} else {
-					fmt.Println("[DEBUG] Successfully removed shared memory segment.")
-				}
-			}()
 
 			clientShmKeyBytes := make([]byte, 4)
 			binary.BigEndian.PutUint32(clientShmKeyBytes, uint32(clientShmKey))
 			_, err = conn.Write([]byte(clientShmKeyBytes))
 			if err != nil {
-				fmt.Printf("[Server] Error writing to client %s: %v\n", clientName, err)
+				fmt.Printf("Error writing to client %s: %v\n", clientName, err)
 				return
 			}
 
@@ -208,30 +175,28 @@ func main() {
 					ShmId:     clientShmId,
 					ShmBuffer: clientShmBuffer,
 				}
-				fmt.Printf("[Server] Registered new client: %s\n", clientName)
+				fmt.Printf("Registered new client: %s\n", clientName)
 			}
 			mu.Unlock()
-
-			clientShmKey += 1
 		}
 	}()
 
-	messageID := 0
 	for {
+		mu.Lock()
+		start := time.Now()
+
+		// Wait for at least one client to connect
 		if len(clients) == 0 {
-			fmt.Println("[driver] No clients yet...")
+			fmt.Println("No clients yet...")
 			time.Sleep(1 * time.Second)
 			continue
 		}
 
-		start := time.Now()
-		dataSize := 10 * 1024 * 1024
-		preState := generatePseudoRandomData(dataSize)
-		copy(shmBuffer, preState)
+		// Generate some input & throw it into the input buffer
+		inputSize := 10 * 1024 * 1024
+		input := generatePseudoRandomData(inputSize)
+		copy(shmBuffer, input)
 
-		messageID++
-
-		mu.Lock()
 		wg := &sync.WaitGroup{}
 		for _, client := range clients {
 			wg.Add(1)
@@ -240,10 +205,10 @@ func main() {
 
 				// Send the message to the client
 				sizeBytes := make([]byte, 4)
-				binary.BigEndian.PutUint32(sizeBytes, uint32(dataSize))
+				binary.BigEndian.PutUint32(sizeBytes, uint32(inputSize))
 				_, err := client.Conn.Write([]byte(sizeBytes))
 				if err != nil {
-					fmt.Printf("[Server] Error writing to client %s: %v\n", client.Name, err)
+					fmt.Printf("Error writing to client %s: %v\n", client.Name, err)
 					mu.Lock()
 					delete(clients, client.Name)
 					mu.Unlock()
@@ -252,17 +217,13 @@ func main() {
 
 				// Wait for a response
 				responseSizeBytes := make([]byte, 4)
-				n, err := client.Conn.Read(responseSizeBytes)
+				_, err = client.Conn.Read(responseSizeBytes)
 				if err != nil {
-					fmt.Printf("[Server] Error reading response from client %s: %v\n", client.Name, err)
+					fmt.Printf("Error reading response from client %s: %v\n", client.Name, err)
 					mu.Lock()
 					delete(clients, client.Name)
 					mu.Unlock()
 					return
-				}
-				if n != 4 {
-					fmt.Printf("Expected 4 bytes, got %d\n", n)
-					os.Exit(1)
 				}
 
 				// Decode the received size
--- processors/rust/main.rs
+++ processors/rust/main.rs
@@ -7,6 +7,8 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Instant;
 use byteorder::{BigEndian, ByteOrder};
+use sha2::Sha256;
+use sha2::Digest;
 
 const SHM_KEY: i32 = 0;
 const SHM_SIZE: usize = 100 * 1024 * 1024;
@@ -14,13 +16,12 @@ const SOCKET_NAME: &str = "/tmp/eth-cl-fuzz";
 
 fn main() {
     // Connect to the Unix domain socket
-    println!("[proc-rust] Connecting to driver...");
+    println!("Connecting to driver...");
     let mut stream = UnixStream::connect(SOCKET_NAME).expect("Failed to connect to driver");
     stream.write_all(b"lighthouse").expect("Failed to send name to driver");
 
-    // Attach to the shared memory segment
+    println!("Attaching to input shared memory: {}", SHM_KEY);
     let shm_read_id = unsafe { shmget(SHM_KEY, SHM_SIZE as size_t, (S_IRUSR | S_IWUSR) as c_int) };
-    println!("[proc-rust] read id: {}", shm_read_id);
     if shm_read_id == -1 {
         panic!("Error getting shared memory segment");
     }
@@ -33,93 +34,75 @@ fn main() {
     let mut shm_key_buffer = [0u8; 4];
     stream.read_exact(&mut shm_key_buffer).expect("Failed to read key from socket");
     let shm_key = BigEndian::read_u32(&shm_key_buffer) as i32;
-    println!("[proc-rust] Received smh key: {}", shm_key);
 
+    println!("Attaching to output shared memory: {}", shm_key);
     let shm_write_id = unsafe { shmget(shm_key, SHM_SIZE as size_t, (S_IRUSR | S_IWUSR) as c_int) };
-    println!("[proc-rust] write id: {}", shm_write_id);
     if shm_write_id == -1 {
         panic!("Error getting shared memory segment");
     }
-
     let shm_write_addr = unsafe { shmat(shm_write_id, ptr::null(), 0) };
     if shm_write_addr == MAP_FAILED {
         panic!("Error attaching to shared memory");
     }
 
-    // Create a shared atomic flag to track if Ctrl+C has been pressed
+    // Create a Ctrl+C handler
     let running = Arc::new(AtomicBool::new(true));
-
-    // Clone the Arc for the signal handler
     let running_clone = Arc::clone(&running);
-
-    // Register a Ctrl+C handler
     ctrlc::set_handler(move || {
-        println!("\n[proc-rust] Ctrl+C detected! Cleaning up...");
+        println!("\nCtrl+C detected! Cleaning up...");
         running_clone.store(false, Ordering::SeqCst);
     }).expect("Error setting Ctrl+C handler");
+    println!("Running... Press Ctrl+C to exit");
 
-    println!("[proc-rust] Running... Press Ctrl+C to exit");
-
-    // Forever loop
+    // The fuzzing loop
     while running.load(Ordering::SeqCst) {
-        // Read the 4-byte size of the data
-        let mut size_buffer = [0u8; 4];
-        match stream.read_exact(&mut size_buffer) {
+        let mut input_size_buffer = [0u8; 4];
+        match stream.read_exact(&mut input_size_buffer) {
             Ok(_) => {
-                let data_size = BigEndian::read_u32(&size_buffer) as usize;
-                println!("[proc-rust] Received data size: {} bytes", data_size);
+                // Get the input
+                let input_size = BigEndian::read_u32(&input_size_buffer) as usize;
+                let input: &[u8] = unsafe { slice::from_raw_parts(shm_read_addr as *const u8, input_size) };
 
-                // Get a mutable slice from the shared memory
-                let input: &[u8] = unsafe { slice::from_raw_parts(shm_read_addr as *const u8, data_size) };
-                let output: &mut [u8] = unsafe { slice::from_raw_parts_mut(shm_write_addr as *mut u8, data_size) };
-
-                // Process the data
+                // Process the input in some way...
                 let start_time = Instant::now();
-                println!("[proc-rust] Processing data...");
-                // Increment each byte in `input` and assign it to `output`
-                for (src, dst) in input.iter().zip(output.iter_mut()) {
-                    *dst = src.wrapping_add(1);
-                }
+                let mut hasher = Sha256::new();
+                hasher.update(input);
+                let result = hasher.finalize();
+                let output_size = result.len();
+                let output: &mut [u8] = unsafe { slice::from_raw_parts_mut(shm_write_addr as *mut u8, output_size) };
+                output.copy_from_slice(&result);
                 let elapsed_time = start_time.elapsed();
-                println!("[proc-rust] Processing time: {:.2?}", elapsed_time);
+                println!("Processing time: {:.2?}", elapsed_time);
 
                 // Send the processed data size back to the driver as 4 bytes
                 let mut response_size_buffer = [0u8; 4];
-                BigEndian::write_u32(&mut response_size_buffer, data_size as u32);
+                BigEndian::write_u32(&mut response_size_buffer, output_size as u32);
                 if let Err(e) = stream.write_all(&response_size_buffer) {
-                    println!("[proc-rust] Failed to send response to driver: {}", e);
+                    println!("Failed to send response to driver: {}", e);
                     break;
                 }
-                println!("[proc-rust] Sent response size to driver");
             }
             Err(e) => {
                 // Break the loop if the driver stops or an error occurs
-                println!("[proc-rust] Failed to read size from socket: {}", e);
+                println!("Failed to read size from socket: {}", e);
                 break;
             }
         }
     }
 
     // Code to execute when the program exits
-    println!("Exiting gracefully. Cleanup completed");
+    println!("Exiting gracefully...");
 
     // Detach from shared memory
     unsafe {
         shmdt(shm_read_addr);
-        shmdt(shm_write_addr);
-
-        // Mark shared memory segments for deletion
         if shmctl(shm_read_id, IPC_RMID, ptr::null_mut()) == -1 {
-            eprintln!("[proc-rust] Failed to remove shared memory (read) with ID: {}", shm_read_id);
-        } else {
-            println!("[proc-rust] Successfully removed shared memory (read) with ID: {}", shm_read_id);
+            eprintln!("Failed to remove shared memory (read) with ID: {}", shm_read_id);
         }
 
+        shmdt(shm_write_addr);
         if shmctl(shm_write_id, IPC_RMID, ptr::null_mut()) == -1 {
-            eprintln!("[proc-rust] Failed to remove shared memory (write) with ID: {}", shm_write_id);
-        } else {
-            println!("[proc-rust] Successfully removed shared memory (write) with ID: {}", shm_write_id);
+            eprintln!("Failed to remove shared memory (write) with ID: {}", shm_write_id);
         }
     };
-    println!("[proc-rust] Detached from shared memory");
 }
